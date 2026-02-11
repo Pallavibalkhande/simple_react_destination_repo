07be7100058b952c7a7fe2099b605336a4070999
Pallavibalkhande <97938109+Pallavibalkhande@users.noreply.github.com>
2026-02-11 12:36:16 +0530

    Delete public/src directory

--- public/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-function App() {
-  return <h1>Hello from Minimal React App!</h1>;
-}
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
